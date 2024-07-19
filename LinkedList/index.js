078c6c4c28ff7b8bc0c8f40589fa9acf1ff5d2f0
SheryarDev <sheryarking5@gmail.com>
2024-07-19 23:32:39 +0500

    added linked list comments

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -6,11 +6,12 @@ class LinkedList {
       value: data,
       next: null,
     };
+    //At start tail and head will be same
     this.tail = this.head;
     this.size = 1;
   }
 
-  //Inser new Node in linkedlis
+  //Inser new Node at the end of linkedlist
   appendNode(nodeData){
     let newNode={
         value:nodeData,
@@ -18,19 +19,19 @@ class LinkedList {
 
     }
 
-   this.tail.next=newNode;
-   this.tail=newNode;
-   this.size+=1;
+   this.tail.next=newNode;// add new Node in the next of existing last node(tail) next
+   this.tail=newNode;  //make newNode the tail of linked list
+   this.size+=1;  // increase the linked list size
   }
 
   //traverse linkedlist
 
   traverse(){
     let counter=0;
-    let currentNode=this.head;
+    let currentNode=this.head; // first node is our start refercence
     while(counter<this.size){
         // console.log("currentNode",currentNode)
-        currentNode=currentNode.next;
+        currentNode=currentNode.next; // iterate the linked list node
         counter++;
     }
     console.log("list",this.head)
@@ -39,20 +40,23 @@ class LinkedList {
   //delete Node
   deleteNode(index){
     let counter=1;
-    let lead=this.head;
+    //lead Node is node that is before the node which we want to delete
+    let leadNode=this.head;
 
     //if node to be deleted is first then replace address of second node in head;
     if(index===1){
         this.head=this.head.next;
     }else{
         while(counter<index-1){
-            lead=lead.next;
+            leadNode=leadNode.next;
             counter++;
         }
        
     }
-    let nextNode=lead.next.next;
-    lead.next=nextNode;
+    //nextnode is node which is after the node which we want to delete.
+    //here we are adding reference of nextNode in leadNode next
+    let nextNode=leadNode.next.next;
+    leadNode.next=nextNode;
     this.size-=1;;
   }
 
